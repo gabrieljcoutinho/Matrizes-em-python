d848612d1ebc0670061082598e2f20309a89a428
Gabriel <gabrieljcoutinho@gmail.com>
2025-08-20 22:04:23 -0300

    novas matrizes

---
+++ Multiplicacao de matrizes.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+A = np.array([1, 2, 3, 4]).reshape(2, 2)
+
+# Multiplicando cada elemento por 3 e atualizando A
+A = 3 * A
+
+print(A)
---
+++ multilplicacao ponto a ponto.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+a = np.array([1.0, 2.0, 3.0])
+b = np.array([2.0, 2.0, 2.0])
+
+print(a * b)
